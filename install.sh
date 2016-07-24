cf272d01a4eed7c6be20a11ab8e7804e10cfbef0
pi @ robot <pi@robot>
2016-07-24 18:33:04 +1000

    initial commit

---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+echo "Install .files"
+
+#Determine install/dotfiles folder
+export DOTFILEPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo ".file Path: $DOTFILEPATH"
+
+#Private Key file creation
+echo ".private creation"
+if [ ! -e $DOTFILEPATH/private ]; then
+  echo "creating private folder"
+  mkdir private
+else
+  echo "private folder already exists"
+fi
+
+if [ ! -e $DOTFILEPATH/private/.private ]; then
+  echo "creating .private"
+  echo "#.private keys" > $DOTFILEPATH/private/.private
+  echo "#Place your private exports / env variables here" >> $DOTFILEPATH/private/.private
+  echo 'echo ".private complete"' >> $DOTFILEPATH/private/.private
+else
+  echo ".private file already exists"
+fi
+
+#Backup existing files
+echo "Create backup of existing .files"
+BACKUPPATH=$DOTFILEPATH/backups/$(date "+%Y%m%d%H%M")
+mkdir -p $BACKUPPATH
+
+if [ -f ~/.bashrc ]; then
+  mv ~/.bashrc $BACKUPPATH/.bashrc.bac
+else
+  echo "No existing .bashrc"
+fi
+
+if [ -f ~/.bash_logout ]; then
+  mv ~/.bash_logout $BACKUPPATH/.bash_logout.bac
+else
+  echo "No existing .bashrc"
+fi
+
+#Symlink to new dotfiles.
+echo "Symlink to new files"
+ln -sfv "$DOTFILEPATH/.bashrc" ~
+ln -sfv "$DOTFILEPATH/.bash_logout" ~
+
+# Finally reload the shell
+echo "Install complete...."
+echo "Reloading shell"
+exec $SHELL -l
---
+++ readme.md
@@ -0,0 +1,19 @@
+# My rPi dotfiles...
+
+This is a repo of the dot files used for my pi devices. Except my .private keys.
+
+This [guide](https://dotfiles.github.io) really helped as did [this](https://medium.com/@webprolific/getting-started-with-dotfiles-43c3602fd789#.qmgph96y5)
+
+The slides in this presentation are also interesting [A storm Homebrewin'](https://speakerdeck.com/anahkiasen/a-storm-homebrewin)
+
+## To install
+Clone repo
+``` bash
+git clone https://github.com/danward79/.dotfiles.git && cd .dotfiles
+```
+then ...
+``` bash
+./install.sh
+```
+
+Private/Secret keys are stored in the .private file in the private file. 
